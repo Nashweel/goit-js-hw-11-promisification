00ab08a4c42d0554fe9d27879a765001f76a9c9f
Oleksii Nesterenko <71211789+Nashweel@users.noreply.github.com>
2021-06-09 10:23:07 +0300

    fixed bugs

--- js/task-2.js
+++ js/task-2.js
@@ -6,7 +6,8 @@ const users = [
   ];
   
   const toggleUserState = (allUsers, userName) => {
-    return new Promise (resolve => {allUsers.map(user =>
+    return new Promise (resolve => {
+      const updatedUsers = allUsers.map(user =>
       user.name === userName ? { ...user, active: !user.active } : user,
     );
   
